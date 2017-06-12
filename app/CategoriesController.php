2182de88765bdedb02896ac978aeef377394d8f0
TheTai <thetai.hvktqs@gmail.com>
2017-06-12 22:24:40 +0700

    commit 3

---
+++ app/CategoriesController.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>NewsController</title>
+</head>
+<body>
+	<h1> Commit 1 </h1>
+	<h1> Commit 2 </h1>
+	<h1> Commit 3 </h1>
+</body>
+</html>
